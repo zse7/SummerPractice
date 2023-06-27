8b5f6dca988df71273e0e7b4a4a38bd4439d2643
zaryanezrya <ivan@sha.run>
2023-06-27 11:35:00 +0600

    Создан ServiceLocator

--- SalonLib/ServiceLocator.cs
+++ SalonLib/ServiceLocator.cs
@@ -1,5 +1,16 @@
 ﻿namespace SalonLib;
 public class ServiceLocator
 {
+    private static IDictionary<string, Func<string>> store;
+
+    static ServiceLocator()
+    {
+        store = new Dictionary<string, Func<string>>();
+    }
+
+    public static string GetService(string key)
+    {
+        return store[key].Invoke();
+    }
 
 }
